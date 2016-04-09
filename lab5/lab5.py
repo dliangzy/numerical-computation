db1972c4a8707d8aea335025e3aa89b1c3985397
liangzy <liuliangshine@gmail.com>
2016-04-09 13:44:06 +0800

    update

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,17 +1,14 @@
 #python
 import numpy as np
 import scipy as sp
-from decimal import *
 
-getcontext().prec=12
-epsilon=pow(0.1,10)
+epsilon=pow(0.1,10)				#define computational accuracy
 def myfunc(x):
 	return pow(x,3)/3-x
 def dmyfunc(x):
 	return x*x-1
 
-def newton(f,df,difx):
-	x_k0=Decimal(difx)
+def newton(f,df,x_k0):			#define newton-iteration function
 	x_k1=x_k0-f(x_k0)/df(x_k0)
 	dalta=1
 	n=1
@@ -21,9 +18,7 @@ def newton(f,df,difx):
 	    x_k1=x_k0-f(x_k0)/df(x_k0)
 	    dalta=abs(x_k1-x_k0)
 	return n, x_k1
-def strfunc(f,x,x1):
-	x_k0=x
-	x_k1=x1
+def secant(f,x_k0,x_k1):			#define secant method function
 	dalta=abs(x_k1-x_k0)
 	n=1
 	x_k1=x_k0-(f(x_k0)*(x_k1-x_k0))/(f(x_k1)-f(x_k0))
@@ -35,14 +30,12 @@ def strfunc(f,x,x1):
 	    n=n+1
 	return n, x_k1
 
-
-
 xlist=[-3.0,-1.5,0.1,0.2,0.75,2.0,6.0]
 for x in xlist:
 	n, y=newton(myfunc,dmyfunc,x)
-	print y, n, x
+	print("%16.12e&%d&%f"%(y ,n,x))
 
 xlist=[-3.0,-1.5,0.1,0.2,0.75,1.0,2.0,6.0]
 for i in range(7):
-	y,n = strfunc(myfunc,xlist[i],xlist[i+1])
-	print y , n, xlist[i], xlist[i+1]
+	y,n = secant(myfunc,xlist[i],xlist[i+1])
+	print("%16.12e & %d & %f & %f"%(n,y,xlist[i],xlist[i+1]))
