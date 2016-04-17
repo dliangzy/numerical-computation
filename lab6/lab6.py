aad1ce0268868ec187f8dc63d7a6197cd6e33f7c
liangzy <liuliangshine@gmail.com>
2016-04-17 14:34:31 +0800

    update

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -13,12 +13,8 @@ def maxelem(A,B,n,m):
 			max_1=A[i,m]
 			sub=i
 	for i in range(n):
-		temp=D(A[m,i])
-		A[m,i]=D(A[sub,i])
-		A[sub,i]=temp
-		btemp=D(B[m])
-		B[m]=D(B[sub])
-		B[sub]=btemp
+		A[m,i],A[sub,i] = A[sub,i],A[m,i]
+		B[m],B[sub] = B[sub], B[m]
 	return A, B
 # make the element below A[m,m] zero
 def elimination(A,B,n,m):
@@ -37,26 +33,29 @@ def result(A,B,n):
 			beta=A[i,j]*x[n-1-j]+beta
 		x.append((B[i]-beta)/A[i,i])
 	return x
-"""
+def elimi(A,B,n):
+	for i in range(n-1):
+		A,B=maxelem(A,B,n,i)
+		A,B=elimination(A,B,n,i)
+	C=result(A,B,n)
+	R=[]
+	for i in range(n-1,-1,-1):
+		R.append(C[i])
+	return R
+###
 # read coefficent matrix from data file
-datpath="array.dat"
-fp = open(datpath)
-A=[]
-for dat in fp:
-	row = [float(x) for x in dat.split()]
-	if len(row)>0:
-		A.append(row)
-A=np.array(A)
-"""
+#datpath="array.dat"
+#fp = open(datpath)
+#A=[]
+#for dat in fp:
+#	row = [float(x) for x in dat.split()]
+#	if len(row)>0:
+#		A.append(row)
+#A=np.array(A)
+###
 # and then, I found a functtion 'loadtxt()'  *_^
 A=np.loadtxt("array.dat")
 B=np.loadtxt("coefficent.dat")
 n=len(B)
-for i in range(n-1):
-	A,B=maxelem(A,B,n,i)
-	A,B=elimination(A,B,n,i)
-C=result(A,B,n)
-R=[]
-for i in range(n-1,-1,-1):
-	R.append(C[i])
+R=elimi(A,B,n)
 np.savetxt("result.dat",R,delimiter="\n")
