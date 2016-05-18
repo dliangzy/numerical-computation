63e7b13a9f07ad6884c4463900ca6869f9e13374
Liang <liuliangshine@gmail.com>
2016-05-18 21:29:35 +0800

    final

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -7,7 +7,7 @@ def func(x,y):
 def Runge_Kutta(func, y, my_range, h):
 	for x in np.arange(my_range[0],my_range[1],h):
 		k_1=func(x,y)
-		k_2=func(x+1/2*h,y+h*k_1/2)
+		k_2=func(x+h/2,y+h*k_1/2)		#mark: "k_2=func(x+h/2,y+h*k/2)" is worry
 		k_3=func(x+h/2,y+h*k_2/2)
 		k_4=func(x+h,y+h*k_3)
 		y=y+h*(k_1+2*k_2+2*k_3+k_4)/6
